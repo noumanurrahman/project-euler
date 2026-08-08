140bc685e14a95bd1cb56dd238036ac65cdf2425
noumanurrahman <hey@noumanrahman.com>
2026-08-08 23:33:30 +0530

    add: problem 003 (largest prime factor)

---
+++ library/numbers.py
@@ -0,0 +1,31 @@
+def find_primes(n:int) -> list[int]:
+    odd_nums = range(2, (n + 1)//2)
+    is_prime = [True for _ in range(n + 1)]
+
+    for i in odd_nums:
+        if is_prime[i]:
+            for j in range(i * i, n + 1, i):
+                is_prime[j] = False
+
+    return [i for i in range(2, n + 1) if is_prime[i]]
+
+def find_prime_factors(n: int) -> list[int]:
+    factors = []
+    divisor = 3
+
+    if n % 2 == 0:
+        factors.append(2)
+        while n % 2 == 0:
+            n //= 2
+
+    while divisor * divisor <= n:
+        if n % divisor == 0:
+            factors.append(divisor)
+            while n % divisor == 0:
+                n //= divisor
+        divisor += 2
+
+    if n > 1:
+        factors.append(n)
+
+    return factors
---
+++ problems/003_largest_prime_factor.py
@@ -0,0 +1,6 @@
+from library.numbers import find_prime_factors
+
+number = 600851475143
+
+factors = find_prime_factors(number)
+print(max(factors))
