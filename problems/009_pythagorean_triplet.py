82a351996e63528b9f1bbedd393c94b652c04fa3
noumanurrahman <hey@noumanrahman.com>
2026-08-10 00:59:50 +0530

    add: problem 009 (pythagorean triplet)

---
+++ problems/009_pythagorean_triplet.py
@@ -0,0 +1,12 @@
+a, b, c = 0, 0, 0
+
+for i in range(1, 999+1):
+    for j in range(1, 999+1):
+        if i == j:
+            continue
+        if i*j - 1000 * (i+j) + 500000 == 0:
+            a, b, c = i, j, 1000 - i - j
+            break
+
+print(a, b, c)
+print(a * b * c)
