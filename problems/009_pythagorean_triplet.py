8737f2016d15c5c4b9f778b3751582e057241bb6
noumanurrahman <hey@noumanrahman.com>
2026-08-13 14:30:08 +0530

    add: problem 011 (product in grid)

--- problems/009_pythagorean_triplet.py
+++ problems/009_pythagorean_triplet.py
@@ -4,6 +4,7 @@ for i in range(1, 997+1):
     for j in range(1, 997+1):
         if i == j:
             continue
+        # see notes/problems/009.md
         if i*j - 1000 * (i+j) + 500000 == 0:
             a, b, c = i, j, 1000 - i - j
             break
